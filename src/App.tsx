8fd4d27d609788ae60fa176c92d1495c4be57566
Pitt Wu <pitt.wu@hlwtech.com.tw>
2022-06-27 18:09:11 +0800

    feat: setting alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Store from './pages/Store';
+import About from './pages/About';
+
 function App() {
-  return <div className="App">Hello App</div>;
+  return (
+    <>
+      <NavBar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}>
+            Home
+          </Route>
+          <Route path="/store" element={<Store />}>
+            Store
+          </Route>
+          <Route path="/about" element={<About />}>
+            About
+          </Route>
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import alias from '@rollup/plugin-alias';
+import { resolve } from 'path';
+
+const projectRootDir = resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), alias()],
+  resolve: {
+    alias: {
+      '@': resolve(projectRootDir, 'src'),
+    },
+  },
+});
